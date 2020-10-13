a9c04f64e2f282ffb7e9bd31cbd4997b3d05a4b5
Andre Morpa <andremorpa@gmail.com>
2020-10-13 11:51:02 +0100

    Change logo in login page

---
+++ admin/src/containers/AuthPage/components/Logo/Img.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+const Img = styled.img`
+  height: 80px;
+  width: 100px;
+`;
+
+export default Img;
---
+++ admin/src/containers/AuthPage/components/Logo/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import LogoStrapi from "../../../../assets/images/black.svg";
+import Img from "./Img";
+
+const Logo = () => <Img src={LogoStrapi} alt="Morpa" />;
+
+export default Logo;
